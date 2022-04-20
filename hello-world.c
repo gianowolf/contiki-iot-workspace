984ef81c42b3e671d28ff3d510d6bc81e1b0564f
Gian Franco Lasala <gianfrancolasala@hotmail.com>
2022-04-20 09:34:20 -0300

    refresh

--- hello-world.c
+++ hello-world.c
@@ -8,15 +8,29 @@
 #include "contiki.h"
 
 #include <stdio.h> /* For printf() */
+
 /*---------------------------------------------------------------------------*/
+
 PROCESS(process_leds, "LEDs Process");
+PROCESS(process_timer, "Timer Process");
+
 AUTOSTART_PROCESSES(&process_leds);
+
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(process_leds, ev, data)
 {
   PROCESS_BEGIN();
 
   printf("RUNNING LEDs Process...\n");
+  
+  PROCESS_END();
+}
+
+PROCESS_THREAD(process_timer, ev, data)
+{
+  PROCESS_BEGIN();
+
+  printf("RUNNING Timer Process...\n");
   
   PROCESS_END();
 }
